534d97d32eeb8473e5f6169eeeba4463eab59d08
Samarth-patel25 <shiv25122006@gmail.com>
2026-01-28 14:42:28 +0530

    tutorial3

--- lab1/polls/views.py
+++ lab1/polls/views.py
@@ -1,23 +1,22 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-from django.http import HttpResponse
-
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def home(request) :
     return HttpResponse("Hello world this is from polls");
     # return render(request,'hello.html');
     
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
 
 
 def results(request, question_id):
@@ -27,3 +26,7 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
